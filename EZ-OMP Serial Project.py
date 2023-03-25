d7828c40968fe9f4dfed6e5ec0245cd49705c4fe
Christian <200390696@student.georgianc.on.ca>
2023-03-24 22:32:00 -0400

    Add functionality to open and read text file

--- EZ-OMP Serial Project.py
+++ EZ-OMP Serial Project.py
@@ -2,8 +2,12 @@
 #Serial Logger for EZ-OMP
 #Capture serial data in real time & create an Excel Spreadsheet
 
+#Goal is to create a serial logger that will parse serial data from COM port directly into .xlsx file. 
+#Will start with basic spreadsheet functionality, then move to parsing existing text file captured from serial, THEN try to add serial read functionality.
+
 #Imports
 import openpyxl #Documentation: https://openpyxl.readthedocs.io/en/stable/
+import os #Documentation: 
 
 #Ask user for the name of the spreadsheet and store it.
 print("Please input the name of the spreadsheet: ")
@@ -12,6 +16,17 @@ spreadsheetName = input()
 #Instantiate a new workbook object, and select the sheet
 workbook1 = openpyxl.Workbook()
 worksheet = workbook1.active
+
+#Open the text file and read the contents
+sourceText = open("CAPTURE.TXT")
+sourceContent = sourceText.read()
+
+#Split the contents of the text file into a list of lines.
+sourceContent.split()
+
+#Iterate over each line to print the contents
+for line in sourceContent:
+    print(sourceContent)
 
 #Add headings:
 worksheet["A1"] = "Time (ms):"
